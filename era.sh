968c21f5f39b02d2d29755c18bb48c0d92d2795a
masam324 <masa13231323@gmail.com>
2022-05-21 18:58:21 +0900

    Modified overall codes and added new feature.

--- era.sh
+++ era.sh
@@ -1,62 +1,42 @@
 #!/bin/bash
 
-#下の""の中にゴミ箱のパスを入力する
-export sot=""
+absoluteTrashPath='/Users/masam/Trash'
+devideCharacter='~'
 
-matchchecker() {
-	searched=`ls -a $sot`
-	for count in $searched; do
-		#matched=`echo $1 | grep ^$count$`
-		if [ "$1" = "$count" ]; then
-			#echo broken changing the name
-			namechanger $1
-			return 0		
-		fi
-	done
-
-	#echo check is completed : $1
-	if [ "$ofn" = "" ]; then
-		eraser $1
-	else
-		eraser $ofn $1
-	fi
-}
-eraser() {
-	if [ "$2" = "" ]; then
-		mv $1 $sot
-	else
-		mv $1 $sot$2	
-	fi
-}
-executor() {
-	#mv $@ ~/.Trash_tmp/
-	for count in $@; do
-		export ofn=""
-		matchchecker $count
-		#echo executed
-	done
-}
-namechanger() {
-	tailname=`echo $1 | grep '[0-9][0-9]*$'`
-	if [ "$tailname" != "" ]; then	#if included number tail of file name
-		tailnumber=`echo $tailname | awk -F'~' '{print $NF}'`
-		tailnumber=`echo $((tailnumber + 1))`
-		name=`echo $tailname | awk -F'~' '{$NF="";print$0}'`
-		name=`echo $name | head -c $((${#name} - 1))`
-		#echo mv $1 ${name}~${tailnumber}
-		#change into mv later
-		checklater=${name}~${tailnumber}
-	else
-		#echo mv $1 $1~2	
-		#also change into mv later
-		checklater=$1~1
-	fi
-	#echo $checklater once check the name
-	if [ "$ofn" == "" ]; then
-		export ofn=$1
-	fi
-	matchchecker $checklater
+initiate(){
+    for objectiveDir in $@; do
+        actualNameDir=`echo $objectiveDir | sed 's/^.*\///g'`
+        while true; do
+            if [ `matchCheck $actualNameDir` -eq 1 ]; then
+                actualNameDir=`changeName $actualNameDir`
+            else
+                echo saved as '"'$actualNameDir'"'
+                break
+            fi
+        done
+        mv $objectiveDir $absoluteTrashPath/$actualNameDir
+    done
 }
 
-executor $@
+matchCheck(){
+    trashDirs=`ls -a $absoluteTrashPath`
+    for trashDir in $trashDirs; do
+        if [ "$trashDir" = "$1" ]; then
+            echo 1
+            return
+        fi
+    done
+    echo 0
+}
 
+changeName() {
+    if [ ! -z `echo $1 | grep "$devideCharacter[0-9]*$"` ]; then
+        withoutNumDir=`echo $1 | sed "s/$devideCharacter[0-9]*$//g"`
+        tailNumDir=`echo $1 | awk -F "$devideCharacter" '{print $NF}'`   
+        echo ${withoutNumDir}${devideCharacter}$((tailNumDir + 1))
+    else
+        echo ${1}${devideCharacter}1 
+    fi
+}
+
+initiate $@
